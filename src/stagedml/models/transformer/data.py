5c58ecf808b6958f9ab561ab1f88ac0d34cf81e1
Sergey Mironov <grrwlf@gmail.com>
2020-03-08 14:17:45 +0300

    Add take arg to transformer's data generators

--- src/stagedml/models/transformer/data.py
+++ src/stagedml/models/transformer/data.py
@@ -7,6 +7,7 @@ from stagedml.models.transformer.imports import ( LearningRateScheduler,
 from stagedml.models.transformer.model import ( create_train_model,
     create_eval_model, create_optimizer )
 
+from typing import Optional
 
 
 def train_ds(params:dict):
@@ -18,7 +19,8 @@ def train_ds(params:dict):
   return train_ds
 
 
-def eval_ds(subtokenizer:Subtokenizer, eval_file:str, batch_size:int, params:dict):
+def predict_ds(subtokenizer:Subtokenizer, eval_file:str, batch_size:int,
+    params:dict, take:Optional[int]=None):
   def _gen():
     with open(eval_file,'r') as f:
       for line in f:
@@ -32,11 +34,34 @@ def eval_ds(subtokenizer:Subtokenizer, eval_file:str, batch_size:int, params:dic
       output_shapes=_repeat(tf.TensorShape([None]))
       )
 
-  # def _filter_max_length(example):
-  #   return tf.size(example['inputs']) <= params['max_length']
-  # ds=ds.filter(_filter_max_length)
+  ds=ds.padded_batch(batch_size=batch_size,
+                     padded_shapes=_repeat([None]),
+                     drop_remainder=False)
+  if take is not None:
+    ds=ds.take(take)
+  return ds
+
+def eval_ds(subtokenizer:Subtokenizer, eval_file:str, target_file:str,
+                                        batch_size:int, params:dict,
+                                        take:Optional[int]=None):
+  def _gen():
+    with open(eval_file,'r') as f,\
+         open(target_file,'r') as t:
+      for line,tline in zip(f,t):
+        yield {'inputs':subtokenizer.encode(line, add_eos=True),
+               'targets':subtokenizer.encode(tline, add_eos=True)}
+
+  def _repeat(x):
+    return {k:x for k in ['inputs','targets']}
+
+  ds=tf.data.Dataset.from_generator(_gen,
+      output_types=_repeat(tf.int64),
+      output_shapes=_repeat(tf.TensorShape([None]))
+      )
 
   ds=ds.padded_batch(batch_size=batch_size,
                      padded_shapes=_repeat([None]),
                      drop_remainder=False)
+  if take is not None:
+    ds=ds.take(take)
   return ds
