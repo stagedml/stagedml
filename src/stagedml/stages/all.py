9c4aea2ddca7b91814238e81f59ee109d33053cc
Sergey Mironov <grrwlf@gmail.com>
2020-03-27 18:59:34 +0300

    Update all, add comments

--- src/stagedml/stages/all.py
+++ src/stagedml/stages/all.py
@@ -38,6 +38,7 @@ all_fetchglue = fetchglue
 all_fetchsquad11 = fetchsquad11
 
 def all_fetchbert(m:Manager)->BertCP:
+  """ Fetch BERT-base pretrained checkpoint from the Google cloud """
   return BertCP(fetchurl(m,
     name='uncased-bert',
     url='https://storage.googleapis.com/cloud-tpu-checkpoints/bert/tf_20/uncased_L-12_H-768_A-12.tar.gz',
@@ -48,52 +49,65 @@ def all_fetchbert(m:Manager)->BertCP:
     ))
 
 def all_glue_tfrecords(m:Manager, task_name:str)->GlueTFR:
+  """ Fetch and preprocess GLUE dataset """
   refbert=all_fetchbert(m)
   refglue=all_fetchglue(m)
   return glue_tfrecords(m, task_name, refbert=refbert, refdataset=refglue)
 
 def all_squad11_tfrecords(m:Manager)->Squad11TFR:
+  """ Fetch and preprocess Squad-1.1 dataset """
   bertref=all_fetchbert(m)
   squadref=all_fetchsquad11(m)
   return squad11_tfrecords(m, bertref, squadref)
 
 def all_bert_finetune_glue(m:Manager, task_name:str)->BertGlue:
+  """ Finetune BERT on GLUE dataset """
   glueref=all_glue_tfrecords(m,task_name)
   return bert_finetune_glue(m,glueref)
 
 def all_bert_finetune_squad11(m:Manager)->BertSquad:
+  """ Finetune BERT on Squad-1.1 dataset """
   squadref=all_squad11_tfrecords(m)
   return bert_finetune_squad11(m,squadref)
 
 # def all_nl2bash(m:Manager)->NL2Bash:
 #   return nl2bash(m)
+
 def all_fetchnl2bash(m:Manager)->DRef:
+  """ Fetch NL2BASH dataset """
   return fetchnl2bash(m)
 
 def all_wmtsubtok_enru(m:Manager)->WmtSubtok:
+  """ Subtokenize En->Ru WMT dataset """
   return wmtsubtok(m, 'en', 'ru')
 
 def all_wmtsubtok_ruen(m:Manager)->WmtSubtok:
+  """ Subtokenize Ru->En WMT dataset """
   return wmtsubtokInv(m, 'ru', 'en')
 
 def all_wmtsubtok_ende(m:Manager)->WmtSubtok:
+  """ Subtokenize En->De WMT dataset """
   return wmtsubtok(m, 'en', 'de')
 
 def all_transformer_wmtenru(m:Manager)->TransWmt:
+  """ Train a Transformer model on WMT En->Ru translation task """
   return transformer_wmt(m, all_wmtsubtok_enru(m))
 
 def all_transformer_wmtruen(m:Manager)->TransWmt:
+  """ Train a Transformer model on WMT Ru->En translation task """
   return transformer_wmt(m, all_wmtsubtok_ruen(m))
 
 def all_nl2bashsubtok(m:Manager, **kwargs)->WmtSubtok:
   return nl2bashSubtok(m, **kwargs)
 
 def all_transformer_nl2bash(m:Manager)->TransWmt:
+  """ Train a Transformer model on NL2Bash dataset """
   return transformer_wmt(m, all_nl2bashsubtok(m))
 
 all_fetchmnist = fetchmnist
 
 def all_convnn_mnist(m:Manager)->ConvnnMnist:
+  """ Train a simple convolutional model on MNIST """
   return convnn_mnist(m, fetchmnist(m))
 
 def all_fetchenwiki(m:Manager)->DRef:
@@ -101,6 +115,8 @@ def all_fetchenwiki(m:Manager)->DRef:
   return extractwiki(m,wikidump)
 
 def gc(force:bool=False)->None:
+  """ Run the garbage collector. Pass `focrce=True` to actually delete
+  the data """
   drefs,rrefs=store_gc(keep_drefs=[], keep_rrefs=\
     filter(lambda x: x is not None, [tryrealize(clo) for clo in [  # type:ignore
       instantiate(all_convnn_mnist),
