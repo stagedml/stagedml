ff31418035a0c94ed0f3b2b716ed7b2ce65eaa57
Sergey Mironov <grrwlf@gmail.com>
2020-04-30 00:26:03 +0300

    Update bert_fintune, more experiments

--- run/bert_finetune/bert_finetune_experiment.py
+++ run/bert_finetune/bert_finetune_experiment.py
@@ -1,16 +1,11 @@
 from stagedml.imports import makedirs
 from stagedml.stages.all import *
 
-
-
-
-
-
-
-from pylightnix import RRef, rref2path, match_some, realizeMany
+from pylightnix import RRef, rref2path, match_some, realizeMany, match_latest
 from stagedml.types import Dict, Union, Optional
 import numpy as np
-from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+from tensorboard.backend.event_processing.event_accumulator import (
+    EventAccumulator, STORE_EVERYTHING_SIZE_GUIDANCE )
 from tensorflow.python.framework.tensor_util import MakeNdarray
 from tensorboard.backend.event_processing.event_accumulator import (
     ScalarEvent, TensorEvent )
@@ -38,12 +33,9 @@ def tensorboard_scalar_events(
   event_acc.Reload()
   return event_acc.Scalars(tag)
 
-def tensorboard_tensor_events(
-    rref:RRef, subfolder:str, tag:str)->ScalarEvent:
+def tensorboard_tensor_events(rref:RRef, subfolder:str, tag:str)->ScalarEvent:
   path=join(rref2path(rref),subfolder)
-  event_acc=EventAccumulator(path, {
-      'compressedHistograms': 10, 'images': 0,
-      'scalars': 100, 'histograms': 1 })
+  event_acc=EventAccumulator(path, {'scalars': 1000, 'tensors':1000 })
   event_acc.Reload()
   return event_acc.Tensors(tag)
 
@@ -69,13 +61,13 @@ def altair_print(chart:Chart, png_filename:str, alt:str='', attrs:str='')->None:
   print("![%s](%s){%s}"%(alt, join(repimgdir,png_filename), attrs))
 
 
-def experiment_bs()->dict:
+def experiment_bs(n:int=1, exclude=[])->Dict[int,List[RRef]]:
   result={}
-  n=5
   for bs in [2,8,16,32,64]:
     def _new_config(cfg:dict):
       cfg['train_batch_size']=bs
       cfg['train_epoches']=5
+      cfg['flags']=[f for f in cfg['flags'] if f not in exclude]
       return mkconfig(cfg)
     result[bs]=realizeMany(instantiate(
       redefine(all_minibert_finetune_glue, new_config=_new_config,
@@ -84,3 +76,30 @@ def experiment_bs()->dict:
   return result
 
 
+def experiment_trainmethod()->Dict[str,RRef]:
+  result={}
+  for tm in ['fit','custom']:
+    def _new_config(cfg:dict):
+      cfg['train_epoches']=5
+      cfg['train_method']=tm
+      return mkconfig(cfg)
+    result[tm]=realize(instantiate(
+      redefine(all_minibert_finetune_glue,
+        new_config=_new_config, new_matcher=match_latest())))
+  return result
+
+
+def experiment_allglue(n:int=1)->Dict[str,List[RRef]]:
+  result={}
+  for task_name in [t for t in glue_tasks() if t.upper() not in ['COLA']]:
+    print(f"Fine-tuning {task_name}")
+    batch_size={'MRPC':8}.get(task_name,32)
+    def _new_config(cfg:dict):
+      cfg['train_batch_size']=batch_size
+      cfg['train_epoches']=3
+      return mkconfig(cfg)
+    result[task_name]=realizeMany(instantiate(
+      redefine(all_minibert_finetune_glue,
+        new_config=_new_config, new_matcher=match_some(n)),
+      task_name=task_name, num_instances=n))
+  return result
